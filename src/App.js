68c27a8b1234fe2b6b71c23589df5c8e55c71ed7
Dylan Gonzalez <dylangonzalez@protonmail.com>
2021-03-05 23:50:14 +1100

    buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <h1>Movie app</h1>
       <h3>All your movie commitment issues solved!</h3>
+      <button>Host Session</button>
+      <button>Join Session</button>
     </div>
   );
 }
