5f3eb8e0ffd448b8f5b19950dd89e21a65819f59
isaacwu6a25 <isaacwu6a25@gmail.com>
2021-03-06 00:57:08 +1100

    styling v2

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ function App() {
     <div className="App">
       <nav className="navbar">
         <a href="/" id="site-title">Movie App</a>
-        <a href="/" id="home">Home</a>
-        <a href="/" id="instructions">Instructions</a>
+        <nav className="navlinks">
+          <a href="/" className="link" id="home">Home</a>
+          <a href="/" className="link" id="instructions">Instructions</a>
+          <a href="/" className="link" id="feedback">Give feedback</a>
+        </nav>
       </nav>
-      <h1>All your movie commitment issues solved!</h1>
+      <div className="body">
+        <h1>The. Syntax. Choice.</h1>
+        <h2>All your movie commitment issues solved!</h2>
+
+      </div>
     </div>
   );
 }
