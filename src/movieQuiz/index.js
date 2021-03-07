6be326972188426418c8531242d821129c08cb98
Dylan Gonzalez <dylangonzalez@protonmail.com>
2021-03-07 12:57:09 +1100

    movie quiz click function

--- src/movieQuiz/index.js
+++ src/movieQuiz/index.js
@@ -3,7 +3,31 @@ import hp from '../movieList/movieImages/harry-potter.jpg'
 import eot from '../movieList/movieImages/edge-of-tomorrow.jpg'
 import mi from '../movieList/movieImages/mission-impossible.jpg'
 
-class MovieQuiz extends React.Component { 
+class MovieQuiz extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            moviesSelected: []
+        }
+
+        this.handleSelection = this.handleSelection.bind(this);
+    }
+
+    async handleChange(event){
+        await this.setState({ moviesSelected: [...this.state.moviesSelected, event.target.value] })
+        console.log(this.state.moviesSelected);
+    }
+
+
+    handleSelection(event){
+        console.log(event.target.value);
+
+        this.setState({ moviesSelected: [...this.state.moviesSelected, event.target.value] })
+        console.log(this.state.moviesSelected);
+
+    }
 
     render() {
         return (
@@ -22,29 +46,58 @@ class MovieQuiz extends React.Component {
                     <div className="forms">
                         <h2 className="form-q">Choose the movie that you would like to watch the most.</h2>
                         <div className="question">
-                            <form className="form-content">
+                            {/* <form className="form-content"> */}
+                            <form onSubmit={this.handleSubmit}>
+
                                 <div className="option opt1">
                                     <label className="container" for="harry-potter">
-                                        <input type="radio" id="harry-potter" name="q1" value="harry-potter"/>
+                                        <input type="radio" id="harry-potter" name="q1" value="harry-potter" onChange={this.handleSelection}/>
                                         <img class="image" src={hp} alt="harry potter"/>
                                     </label>
                                     <p className="descriptions">Harry Potter</p>
                                 </div>
                                 <div className="option opt2">
                                     <label className="container" for="edge-of-tomorrow">
-                                        <input type="radio" id="edge-of-tomorrow" name="q1" value="edge-of-tomorrow"/>
+                                        <input type="radio" id="edge-of-tomorrow" name="q1" value="edge-of-tomorrow" onChange={this.handleSelection}/>
                                         <img class="image" src={eot} alt="edge of tomorrow"/>
                                     </label>
                                     <p className="descriptions">Edge of Tomorrow</p>
                                 </div>
                                 <div className="option opt3">
                                     <label className="container" for="mission-impossible">
-                                        <input type="radio" id="mission-impossible" name="q1" value="mission-impossible"/>
+                                        <input type="radio" id="mission-impossible" name="q1" value="mission-impossible" onChange={this.handleSelection}/>
                                         <img class="image" src={mi} alt="mission impossible"/>
                                     </label>
                                     <p className="descriptions">Mission Impossible</p>
+                                    </div>
+                                </form>
+                                
+                            <form onSubmit={this.handleSubmit}>
+
+                                <div className="option opt1">
+                                    <label className="container" for="harry-potter">
+                                        <input type="radio" id="harry-potter" name="q1" value="harry-potter" onChange={this.handleSelection}/>
+                                        <img class="image" src={hp} alt="harry potter"/>
+                                    </label>
+                                    <p className="descriptions">Harry Potter</p>
                                 </div>
-                            </form>
+                                <div className="option opt2">
+                                    <label className="container" for="edge-of-tomorrow">
+                                        <input type="radio" id="edge-of-tomorrow" name="q1" value="edge-of-tomorrow" onChange={this.handleSelection}/>
+                                        <img class="image" src={eot} alt="edge of tomorrow"/>
+                                    </label>
+                                    <p className="descriptions">Edge of Tomorrow</p>
+                                </div>
+                                <div className="option opt3">
+                                    <label className="container" for="mission-impossible">
+                                        <input type="radio" id="mission-impossible" name="q1" value="mission-impossible" onChange={this.handleSelection}/>
+                                        <img class="image" src={mi} alt="mission impossible"/>
+                                    </label>
+                                    <p className="descriptions">Mission Impossible</p>
+                                    </div>
+                                </form>
+
+                            {/* </form> */}
                         </div>
                     </div> 
                 </div>
